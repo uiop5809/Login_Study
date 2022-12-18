30dd58de0abe256c50ac9e76665c002e5fa5e54e
parkyejin <uiop5809@naver.com>
2022-12-19 01:20:35 +0900

    CORS 해결, Proxy 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 export default function LandingPage() {
+  useEffect(() => {
+    axios.get("/api/hello").then((res) => console.log(res.data));
+  }, []);
+
   return <div>LandingPage</div>;
 }
