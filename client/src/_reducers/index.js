92b83fe6d28cf20fdccfd4c448242cdbb4d9f725
parkyejin <uiop5809@naver.com>
2022-12-19 14:52:45 +0900

    Provider store 설정

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+// import user from "./user_reducer";
+// import comment from "./comment_reducer";
+
+// 여러개의 reducer를 하나로 합쳐준다.
+const rootReducer = combineReducers({
+  // user,
+  // comment,
+});
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import "antd/dist/antd.css";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise"; // promise를 사용하기 위한 미들웨어
+import ReduxThunk from "redux-thunk"; // 비동기를 사용하기 위한 미들웨어
+import Reducer from "./_reducers/index.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 원래 store는 객체밖에 못받기 때문에 promise와 function을 사용하기 위해 미들웨어를 사용한다.
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__() // redux devtools를 사용하기 위한 코드
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
